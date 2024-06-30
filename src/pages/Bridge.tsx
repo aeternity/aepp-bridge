fe7c69d0f19a9d486de0f59a9642667de0305da8
Yusuf Seyrek <seyrek91@gmail.com>
2024-06-30 21:14:11 +0300

    refactor: dialogs & display bridge action summary

--- src/pages/Bridge.tsx
+++ src/pages/Bridge.tsx
@@ -5,16 +5,15 @@ import {
     Breadcrumbs,
     Container,
     Dialog,
+    DialogActions,
     DialogContent,
     DialogTitle,
     Divider,
     Grid,
-    Slide,
-    SlideProps,
-    Snackbar,
     Stack,
     Typography,
 } from '@mui/material';
+import CheckIcon from '@mui/icons-material/CheckCircleOutline';
 import AeternityIcon from 'src/components/base/icons/aeternity';
 import EthereumIcon from 'src/components/base/icons/ethereum';
 
@@ -37,6 +36,8 @@ import * as Ethereum from 'src/services/ethereum';
 import WalletConnection from 'src/components/base/WalletConnection';
 import { AeternityAssetInfo, EthereumAssetInfo, Direction } from 'src/context/AppContext';
 import Spinner from 'src/components/base/Spinner';
+import { useSnackbar } from 'notistack';
+import BigNumber from 'bignumber.js';
 
 const BRIDGE_TOKEN_ACTION_TYPE = 0;
 const BRIDGE_ETH_ACTION_TYPE = 1;
@@ -64,18 +65,30 @@ const printBalance = (
     return symbol;
 };
 
-function SlideTransition(props: SlideProps) {
-    return <Slide {...props} direction="left" />;
+interface BridgeAction {
+    direction: Direction;
+    asset: Asset;
+    amount: string;
+    destination: string;
+    allowanceTxHash: string;
+    bridgeTxHash: string;
 }
 
+const getTxUrl = (direction: Direction, hash: string) => {
+    return direction === Direction.AeternityToEthereum
+        ? `${Constants.aeternity.explorer}/transactions/${hash}`
+        : `${Constants.ethereum.etherscan}/tx/${hash}`;
+};
+
 const Bridge: React.FC = () => {
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
     const { aeternity, ethereum, assets, asset, updateAsset, direction, updateDirection } = useAppContext();
     const { aeternityAddress, ethereumAddress } = useWalletContext();
-    const [error, setError] = React.useState('');
+
     const [buttonBusy, setButtonBusy] = React.useState(false);
     const [confirming, setConfirming] = React.useState(false);
     const [confirmingMsg, setConfirmingMsg] = React.useState('');
-    const [operationHash, setOperationHash] = React.useState('');
+    const [bridgeActionSummary, setBridgeActionSummary] = React.useState<BridgeAction | null>(null);
 
     const [destination, setDestination] = React.useState<string>();
     const [amount, setAmount] = React.useState<string>();
@@ -123,6 +136,29 @@ const Bridge: React.FC = () => {
         return Aeternity.isAddressValid(destination);
     }, [destination, direction]);
 
+    const showTransactionSubmittedMessage = (message: string, hash: string) =>
+        enqueueSnackbar(message, {
+            variant: 'info',
+            anchorOrigin: { vertical: 'top', horizontal: 'right' },
+            action: () => {
+                const url = getTxUrl(direction, hash);
+                return (
+                    <Button variant="text" color="inherit" onClick={() => (window as any).open(url, '_blank').focus()}>
+                        View
+                    </Button>
+                );
+            },
+        });
+
+    const showErrorMessage = (message: string) => {
+        message &&
+            enqueueSnackbar(message.substring(0, 50), {
+                variant: 'error',
+                anchorOrigin: { vertical: 'top', horizontal: 'right' },
+                autoHideDuration: 4000,
+            });
+    };
+
     const bridgeToAeternity = React.useCallback(async () => {
         const bridge = new Ethereum.Contract(
             Constants.ethereum.bridge_address,
@@ -135,19 +171,20 @@ const Bridge: React.FC = () => {
             Ethereum.Provider.getSigner(),
         );
         if (!isValidDestination || !destination?.startsWith('ak_')) {
-            return setError('Invalid destination!');
+            return showErrorMessage('Invalid destination!');
         }
         if (!normalizedAmount || normalizedAmount <= 0) {
-            return setError('Invalid amount!');
+            return showErrorMessage('Invalid amount!');
         }
         if (normalizedAmount > Number(ethereum.assetInfo?.asset?.balance || 0)) {
-            return setError('Not enough balance!');
+            return showErrorMessage('Not enough balance!');
         }
 
         setButtonBusy(true);
 
         let action_type = BRIDGE_TOKEN_ACTION_TYPE;
         let eth_amount = BigInt(0);
+        let allowanceTxHash = '';
         if (asset.ethAddress === Constants.ethereum.default_eth) {
             action_type = BRIDGE_ETH_ACTION_TYPE;
             eth_amount = BigInt(normalizedAmount);
@@ -157,20 +194,21 @@ const Bridge: React.FC = () => {
             try {
                 const allowance = await assetContract.allowance(ethereumAddress, Constants.ethereum.bridge_address);
                 if (allowance.lt(normalizedAmount)) {
+                    setConfirming(true);
+                    setConfirmingMsg('Approving allowance');
                     const approveResult = await assetContract.approve(
                         Constants.ethereum.bridge_address,
                         normalizedAmount,
                     );
-                    setOperationHash(approveResult.hash);
-                    setConfirmingMsg('Approving allowance');
-                    setConfirming(true);
+
+                    allowanceTxHash = approveResult.hash;
+                    showTransactionSubmittedMessage('Allowance transaction submitted.', approveResult.hash);
 
                     await approveResult.wait(1);
-                    setConfirming(false);
                 }
             } catch (e: any) {
                 Logger.error(e);
-                setError(e.message);
+                showErrorMessage(e.message);
             } finally {
                 setConfirming(false);
                 setConfirmingMsg('');
@@ -178,6 +216,8 @@ const Bridge: React.FC = () => {
         }
 
         try {
+            setConfirming(true);
+            setConfirmingMsg('Bridge action');
             const bridgeOutResult = await bridge.bridge_out(
                 asset.ethAddress,
                 destination,
@@ -187,37 +227,43 @@ const Bridge: React.FC = () => {
                     value: eth_amount,
                 },
             );
-            setOperationHash(bridgeOutResult.hash);
-            setConfirmingMsg('Bridge action');
-            setConfirming(true);
+
+            setBridgeActionSummary({
+                direction,
+                asset,
+                destination,
+                amount: new BigNumber(normalizedAmount.toString()).shiftedBy(-asset.decimals).toString(),
+                allowanceTxHash,
+                bridgeTxHash: bridgeOutResult.hash,
+            });
 
             await bridgeOutResult.wait(1);
         } catch (e: any) {
             Logger.error(e);
-            setError(e.message);
+            showErrorMessage(e.message);
         } finally {
             setConfirming(false);
             setConfirmingMsg('');
-            setOperationHash('');
         }
         setButtonBusy(false);
     }, [asset, ethereum, destination, normalizedAmount, isValidDestination]);
 
     const bridgeToEvm = React.useCallback(async () => {
         if (!isValidDestination || !destination?.startsWith('0x')) {
-            return setError('Invalid destination!');
+            return showErrorMessage('Invalid destination!');
         }
         if (!normalizedAmount || normalizedAmount <= 0) {
-            return setError('Invalid amount!');
+            return showErrorMessage('Invalid amount!');
         }
         if (normalizedAmount > Number(aeternity.assetInfo?.asset?.balance || 0)) {
-            return setError('Not enough balance!');
+            return showErrorMessage('Not enough balance!');
         }
 
         setButtonBusy(true);
         try {
             let action_type = BRIDGE_TOKEN_ACTION_TYPE;
             let ae_amount = BigInt(0);
+            let allowanceTxHash = '';
 
             if (asset.aeAddress === Constants.aeternity.default_ae) {
                 action_type = BRIDGE_AETERNITY_ACTION_TYPE;
@@ -239,17 +285,21 @@ const Bridge: React.FC = () => {
                 if (allowance === undefined) {
                     setConfirmingMsg('Creating allowance');
                     setConfirming(true);
-                    await asset_contract.create_allowance(
+                    const allowanceCall = await asset_contract.create_allowance(
                         Constants.aeternity.bridge_address.replace('ct_', 'ak_'),
                         normalizedAmount,
                     );
+                    allowanceTxHash = allowanceCall.hash;
+                    showTransactionSubmittedMessage('Allowance transaction submitted.', allowanceCall.hash);
                 } else if (Number(allowance) < Number(normalizedAmount)) {
                     setConfirmingMsg('Updating allowance');
                     setConfirming(true);
-                    await asset_contract.change_allowance(
+                    const allowanceCall = await asset_contract.change_allowance(
                         Constants.aeternity.bridge_address.replace('ct_', 'ak_'),
                         normalizedAmount,
                     );
+                    allowanceTxHash = allowanceCall.hash;
+                    showTransactionSubmittedMessage('Allowance transaction submitted.', allowanceCall.hash);
                 }
                 setConfirming(false);
                 setConfirmingMsg('');
@@ -266,14 +316,20 @@ const Bridge: React.FC = () => {
                 [asset.ethAddress, destination, normalizedAmount, action_type],
                 { amount: ae_amount },
             );
-            setOperationHash(bridge_out_call.hash);
+            setBridgeActionSummary({
+                direction,
+                asset,
+                destination,
+                amount: new BigNumber(normalizedAmount.toString()).shiftedBy(-asset.decimals).toString(),
+                allowanceTxHash,
+                bridgeTxHash: bridge_out_call.hash,
+            });
         } catch (e: any) {
             Logger.error(e);
-            setError(e.message);
+            showErrorMessage(e.message);
         } finally {
             setConfirming(false);
             setConfirmingMsg('');
-            setTimeout(() => setOperationHash(''), 5000);
         }
 
         setButtonBusy(false);
@@ -446,7 +502,7 @@ const Bridge: React.FC = () => {
                     </Grid>
                     <CardActions sx={{ margin: 1, paddingTop: 1 }}>
                         <WalletConnection
-                            onWalletConnectError={setError}
+                            onWalletConnectError={showErrorMessage}
                             requiredWallet={
                                 direction == Direction.EthereumToAeternity
                                     ? RequiredWallet.Ethereum
@@ -467,43 +523,82 @@ const Bridge: React.FC = () => {
                 </Card>
             </Grid>
 
-            {!!error && (
-                <Snackbar
-                    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-                    open={true}
-                    autoHideDuration={4000}
-                    onClose={() => setError('')}
-                    TransitionComponent={SlideTransition}
-                    sx={{ mt: 8 }}
-                >
-                    <Alert onClose={() => setError('')} severity="error" variant="standard" sx={{ width: '100%' }}>
-                        {error.includes('ACTION_REJECTED') ? 'User rejected transaction' : error}
-                    </Alert>
-                </Snackbar>
-            )}
-            <Dialog title="Operation Hash" open={!!operationHash} onClose={() => setOperationHash('')} maxWidth="md">
-                <DialogTitle>Transaction submitted</DialogTitle>
+            <Dialog disableEscapeKeyDown={true} open={!!bridgeActionSummary} maxWidth="md">
+                <DialogTitle>Bridge action summary</DialogTitle>
                 <DialogContent>
-                    {direction === Direction.AeternityToEthereum ? (
-                        <a
-                            style={{ color: 'black' }}
-                            target="_blank"
-                            href={`${Constants.aeternity.explorer}/transactions/${operationHash}`}
-                            rel="noreferrer"
-                        >
-                            Check operation on AeScan
-                        </a>
-                    ) : (
-                        <a
-                            style={{ color: 'black' }}
-                            target="_blank"
-                            href={`${Constants.ethereum.etherscan}/tx/${operationHash}`}
-                            rel="noreferrer"
-                        >
-                            Check operation on Etherscan
-                        </a>
-                    )}
+                    <Grid container flexDirection={'column'} rowGap={1}>
+                        <Grid flexDirection={'row'} container justifyContent={'space-between'}>
+                            <Grid item>You have successfully submitted a bridge transaction</Grid>
+                            <Grid item>
+                                <CheckIcon color="success" />
+                            </Grid>
+                        </Grid>
+
+                        <Grid flexDirection={'row'} container justifyContent={'space-between'}>
+                            <Grid>From:</Grid>
+                            <Grid>
+                                {bridgeActionSummary?.direction === Direction.AeternityToEthereum
+                                    ? 'Aeternity to Ethereum'
+                                    : 'Ethereum to Aeternity'}
+                            </Grid>
+                        </Grid>
+
+                        <Grid flexDirection={'row'} container justifyContent={'space-between'}>
+                            <Grid>Amount:</Grid>
+                            <Grid>
+                                {bridgeActionSummary?.amount} {bridgeActionSummary?.asset.symbol}
+                            </Grid>
+                        </Grid>
+
+                        <Grid flexDirection={'row'} container justifyContent={'space-between'}>
+                            <Grid>Destination:</Grid>
+                            <Grid>{bridgeActionSummary?.destination}</Grid>
+                        </Grid>
+
+                        {bridgeActionSummary?.allowanceTxHash && (
+                            <Grid flexDirection={'row'} container justifyContent={'space-between'}>
+                                <Grid>Allowance transaction:</Grid>
+                                <Grid>
+                                    <a
+                                        style={{ color: 'black' }}
+                                        target="_blank"
+                                        href={getTxUrl(
+                                            bridgeActionSummary?.direction!,
+                                            bridgeActionSummary?.allowanceTxHash!,
+                                        )}
+                                    >
+                                        View allowance transaction on{' '}
+                                        {bridgeActionSummary?.direction === Direction.AeternityToEthereum
+                                            ? 'etherscan'
+                                            : 'aescan'}
+                                    </a>
+                                </Grid>
+                            </Grid>
+                        )}
+                        <Grid flexDirection={'row'} container justifyContent={'space-between'}>
+                            <Grid>Bridge transaction:</Grid>
+                            <Grid>
+                                <a
+                                    style={{ color: 'black' }}
+                                    target="_blank"
+                                    href={getTxUrl(bridgeActionSummary?.direction!, bridgeActionSummary?.bridgeTxHash!)}
+                                >
+                                    View bridge transaction on{' '}
+                                    {bridgeActionSummary?.direction === Direction.AeternityToEthereum
+                                        ? 'etherscan'
+                                        : 'aescan'}
+                                </a>
+                            </Grid>
+                        </Grid>
+                        <Grid marginTop={3} textAlign={'center'}>
+                            Your tokens will be available in the destination network after the transaction is confirmed
+                            and processed.
+                        </Grid>
+                    </Grid>
                 </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setBridgeActionSummary(null)}>Close</Button>
+                </DialogActions>
             </Dialog>
         </Container>
     );
