bfdc96bf6ea3e8ca10c37d126facd0974a59532d
Yusuf Seyrek <seyrek91@gmail.com>
2024-09-13 18:40:07 +0300

    feat: adjust content height for page container component #62

--- src/components/base/PageContainer.tsx
+++ src/components/base/PageContainer.tsx
@@ -7,15 +7,39 @@ interface Props {
 }
 
 const PageContainer = (props: Props) => {
+    const scrollableHeightForDesktop = `calc(100vh - 256px)`;
+    const scrollableHeightForMobile = `calc(100vh - 308px)`;
+
     return (
         <Container sx={{ paddingY: 8 }}>
             <Grid container direction="row" justifyContent="center" alignItems="flex-start">
                 <Card sx={props.cardSx}>
-                    <CardContent>
+                    <CardContent
+                        sx={{
+                            maxHeight: {
+                                xs: scrollableHeightForMobile,
+                                sm: scrollableHeightForMobile,
+                                md: scrollableHeightForDesktop,
+                                lg: scrollableHeightForDesktop,
+                            },
+                            display: 'flex',
+                            flexDirection: 'column',
+                        }}
+                    >
                         <Typography variant="h4" gutterBottom p={1}>
                             {props.title}
                         </Typography>
-                        {props.children}
+                        <div
+                            style={{
+                                overflowY: 'scroll',
+                                overflowX: 'hidden',
+                                display: 'flex',
+                                flex: 1,
+                                flexDirection: 'column',
+                            }}
+                        >
+                            {props.children}
+                        </div>
                     </CardContent>
                 </Card>
             </Grid>
